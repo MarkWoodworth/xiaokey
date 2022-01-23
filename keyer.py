83cf2e3b5ed4afe365b878350b6bf5438e7ed77d
mark <mark@woodworths.net>
2022-01-23 16:43:36 -0600

    first

---
+++ keyer.py
@@ -0,0 +1,331 @@
+# keyer.py
+#
+# (C) Copyright 2022 Mark Woodworth AC9YW All Rights Reserved
+#
+#               seed xiao rp2040
+#
+#                 +---USB C--+  
+#  paddle dit in  D0        5V
+#  paddle dah in  D1       GND
+#         key out D2       3v3
+#      buzzer out D3       D10 in  button 1
+#     i2c sda     D4        D9 in  button 2 
+#     i2c scl     D5        D8 in  button 3
+#          tx     D6        D7     rx
+#                 +----------+
+
+import time
+import board
+import pwmio
+import neopixel
+from digitalio import DigitalInOut, Direction, Pull
+
+import usb_cdc
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+# configuration
+WPM = 15
+SIDETONE = True
+SIDEFREQ = 880
+KEYBOARD = False
+
+# messages
+MSG1 = "AC9YW"
+MSG2 = "CQ CQ CQ DE AC9YW AC9YW AC9YW K"
+MSG3 = "73"
+
+
+# setup leds
+pixel = neopixel.NeoPixel(board.NEOPIXEL,1)
+red   = DigitalInOut(board.LED_RED)
+red.direction = Direction.OUTPUT
+red.value = True
+green = DigitalInOut(board.LED_GREEN)
+green.direction = Direction.OUTPUT
+green.value = True
+blue  = DigitalInOut(board.LED_BLUE)
+blue.direction = Direction.OUTPUT
+blue.value = True
+
+# setup buzzer
+buzzer = pwmio.PWMOut(board.D3,variable_frequency=True)
+buzzer.frequency = SIDEFREQ
+OFF = 0
+ON = 2**15
+
+# setup keyer output
+key = DigitalInOut(board.D2) ;
+key.direction = Direction.OUTPUT
+
+# setup paddle inputs
+dit_key = DigitalInOut(board.D1)
+dit_key.direction = Direction.INPUT
+dit_key.pull = Pull.UP
+dah_key = DigitalInOut(board.D0)
+dah_key.direction = Direction.INPUT
+dah_key.pull = Pull.UP
+
+# setup push buttons
+b1 = DigitalInOut(board.D10)
+b1.direction = Direction.INPUT
+b1.pull = Pull.UP
+b2 = DigitalInOut(board.D9)
+b2.direction = Direction.INPUT
+b2.pull = Pull.UP
+b3 = DigitalInOut(board.D8)
+b3.direction = Direction.INPUT
+b3.pull = Pull.UP
+
+# setup usb serial
+serial = usb_cdc.data
+
+# setup keyboard output
+time.sleep(1)  
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+
+# setup encode and decode
+encodings = {}
+def encode(char):
+    global encodings
+    if char in encodings:
+        return encodings[char]
+    elif char.lower() in encodings:
+        return encodings[char.lower()]
+    else:
+        return ''
+
+decodings = {}
+def decode(char):
+    global decodings
+    if char in decodings:
+        return decodings[char]
+    else:
+        return '('+char+'?)'
+
+def morse(pattern,letter):
+    decodings[pattern] = letter
+    encodings[letter ] = pattern
+    
+morse('.-'   ,'a')
+morse('-...' ,'b')
+morse('-.-.' ,'c')
+morse('-..'  ,'d')
+morse('.'    ,'e')
+morse('..-.' ,'f')
+morse('--.'  ,'g')
+morse('....' ,'h')
+morse('..'   ,'i')
+morse('.---' ,'j')
+morse('-.-'  ,'k')
+morse('.-..' ,'l')
+morse('--'   ,'m')
+morse('-.'   ,'n')
+morse('---'  ,'o')
+morse('.--.' ,'p')
+morse('--.-' ,'q')
+morse('.-.'  ,'r')
+morse('...'  ,'s')
+morse('-'    ,'t')
+morse('..-'  ,'u')
+morse('...-' ,'v')
+morse('.--'  ,'w')
+morse('-..-' ,'x')
+morse('-.--' ,'y')
+morse('--..' ,'z')
+              
+morse('.----','1')
+morse('..---','2')
+morse('...--','3')
+morse('....-','4')
+morse('.....','5')
+morse('-....','6')
+morse('--...','7')
+morse('---..','8')
+morse('----.','9')
+morse('-----','0')
+
+morse('...---...','!')
+
+# key down and up
+def cw(on):
+    if on:
+        key.value = True
+        if SIDETONE:
+           buzzer.duty_cycle = ON
+        pixel.fill((0,0,30))
+    else:
+        key.value = False
+        buzzer.duty_cycle = OFF
+        pixel.fill((0,0,0))
+        
+# timing
+def dit_time():
+    global WPM
+    PARIS = 50 
+    return 60.0 / WPM / PARIS
+
+# transmit strings
+def play(pattern):
+    for sound in pattern:
+        if sound == '.':
+            cw(True)
+            time.sleep(dit_time())
+            cw(False)
+            time.sleep(dit_time())
+        elif sound == '-':
+            cw(True)
+            time.sleep(3*dit_time())
+            cw(False)
+            time.sleep(dit_time())
+        elif sound == ' ':
+            time.sleep(4*dit_time())
+    time.sleep(2*dit_time())
+    
+def xmit(message):
+    for letter in message:
+        send(letter)
+        play(encode(letter))
+    play(' ')
+    
+def serials():
+    if serial.connected:
+        if serial.in_waiting > 0:
+            letter = serial.read().decode('utf-8')
+            send(letter)
+            play(encode(letter))
+            
+def send(c):
+#    print(c,end='')
+    if serial.connected:
+        serial.write(str.encode(c))
+    if KEYBOARD:
+        keyboard_layout.write(c)
+    
+class Iambic:
+    def __init__(self,dit_key,dah_key):
+        self.dit_key = dit_key
+        self.dah_key = dah_key
+        self.dit = False
+        self.dah = False
+        self.SPACE    = 0
+        self.DIT      = 1
+        self.DIT_WAIT = 2
+        self.DAH      = 3
+        self.DAH_WAIT = 4
+        self.state = self.SPACE
+        self.old_state = -1
+        self.in_char = False
+        self.in_word = False
+        self.start = 0
+        self.char = ''
+    def hack(self):
+        self.start = time.monotonic()
+    def elapsed(self):
+        return time.monotonic() - self.start
+    def latch_paddles(self):
+        if not self.dit_key.value:
+            self.dit = True
+        if not self.dah_key.value:
+            self.dah = True
+    def cycle(self):
+        self.latch_paddles()
+        if self.state == self.SPACE:
+            if self.dit:
+                self.dit= False
+                self.dah = False
+                self.char += "."
+                self.state = self.DIT
+                cw(True)
+                self.hack()
+                self.in_char = True
+                self.in_word = True
+            elif self.dah:
+                self.dit = False
+                self.dah = False
+                self.char += '-'
+                self.state = self.DAH
+                cw(True)
+                self.hack()
+                self.in_char = True
+                self.in_word = True
+            elif self.in_char and self.elapsed()>2*dit_time():
+                self.in_char = False
+                send(decode(self.char))
+                self.char = ""
+            elif self.in_word and self.elapsed()>6*dit_time():
+                self.in_word = False
+                send(" ") 
+        elif self.state == self.DIT:
+            if self.elapsed() > dit_time():
+                cw(False)
+                self.hack()
+                self.dit = False
+                self.state = self.DIT_WAIT
+        elif self.state == self.DIT_WAIT:
+            if self.elapsed() > dit_time():
+                if self.dah:
+                    self.dah = False
+                    self.dit = False
+                    self.char += '-'
+                    cw(True)
+                    self.hack()
+                    self.state = self.DAH
+                elif self.dit:
+                    self.dah = False
+                    self.dit = False
+                    self.char += '.'
+                    cw(True)
+                    self.hack()
+                    self.state = self.DIT
+                else:
+                    self.hack()
+                    self.state = self.SPACE
+        elif self.state == self.DAH:
+            if self.elapsed() > 3*dit_time():
+                self.dah = False
+                cw(False)
+                self.hack()
+                self.state = self.DAH_WAIT
+        elif self.state == self.DAH_WAIT:
+            if self.elapsed() > dit_time():
+                if self.dit:
+                    self.dah = False
+                    self.dit = False
+                    self.char += '.'
+                    cw(True)
+                    self.hack()
+                    self.state = self.DIT
+                elif self.dah:
+                    self.dah = False
+                    self.dit = False
+                    self.char += '-'
+                    cw(True)
+                    self.hack()
+                    self.state = self.DAH
+                else:
+                    self.hack()
+                    self.state = self.SPACE              
+
+iambic = Iambic(dit_key,dah_key)
+
+def buttons():
+    global b1, b2, b3
+    if not b1.value:
+        xmit(MSG1)
+    if not b2.value:
+        xmit(MSG2)
+    if not b3.value:
+        xmit(MSG3)
+        
+green.value = False
+
+while True:
+    buttons()
+    serials()
+    iambic.cycle()
+
+            
